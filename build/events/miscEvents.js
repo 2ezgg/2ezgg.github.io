9c8d1ab5b2a522a13aa21fc2e8251f47312cf5d2
Joel Olsson <joel_1st@hotmail.com>
2014-05-25 00:01:40 +1000

    Recent version of chrome has destroyed functionality of back button when advertisements reload - therefore all advertisements are disabled except on initial load.

--- build/events/miscEvents.js
+++ build/events/miscEvents.js
@@ -10,7 +10,7 @@ $("#sidebar").on('click', '.nav-button', function(e){
     $(this).children('li').addClass('selected-link');
     ga('send', 'pageview', "#" + web.hashWithoutParams());
   }
-  $('#advertisement').html('<iframe src="http://ib.adnxs.com/tt?id=2359794&referrer=2ez.gg" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" topmargin="0" leftmargin="0" allowtransparency="true" width="300" height="250"></iframe>');
+  //$('#ezggadvertisement').html(' ').html('<iframe src="http://ib.adnxs.com/tt?id=2359794&referrer=2ez.gg" sandbox="allow-scripts" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" topmargin="0" leftmargin="0" allowtransparency="true" width="300" height="250"></iframe>');
 });
 
 $('.tooltip').tipsy({gravity: 'w'});
